7e6d607b3760cf0084200c2dfba70a8118e45346
Rishikeshningad <ningadr@gmail.com>
2021-11-02 04:41:34 +0530

    getelementbyclassname

--- dom.js
+++ dom.js
@@ -18,10 +18,28 @@
 // GETELEMENTBYID //
 // console.log(document.getElementById('header-title'));
 // var headerTitle = document.getElementById('header-title');
- var header = document.getElementById('main-header');
+// var header = document.getElementById('main-header');
 // console.log(headerTitle);
 // headerTitle.textContent = 'Hello';
 // headerTitle.innerText = 'Goodbye';
 // console.log(headerTitle.innerText);
 // headerTitle.innerHTML = '<h3>Hello</h3>';
-header.style.borderBottom = 'solid 3px #000';
+//header.style.borderBottom = 'solid 3px #000';
+
+// GETELEMENTSBYCLASSNAME //
+ var items = document.getElementsByClassName('list-group-item');
+ console.log(items);
+ console.log(items[1]);
+ //items[1].textContent = 'Hello 2';
+ //items[1].style.fontWeight = 'bold';
+ items[2].style.backgroundColor = 'green';
+
+// // Gives error
+// //items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < items.length; i++){
+//   items[i].style.backgroundColor = '#f4f4f4';
+// }
+for(var i = 0; i < items.length; i++){
+   items[i].style.fontWeight = 'bold';
+ }
