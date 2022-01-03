41ec6538a229156aee28c43942c579fd81f60e22
Rishikeshningad <ningadr@gmail.com>
2022-01-03 20:47:25 +0530

    request and response

--- first-app.js
+++ first-app.js
@@ -1,8 +1,26 @@
 const http = require('http');
 const server = http.createServer((req,res) => {
-     console.log(req);
-     res.write('Rishikesh')
-     res.end()
+    const url = req.url;
+    if (url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>My first Page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();  
+    } else if (url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>My first Page</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();  
+    } else if (url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        res.end(); 
+    }
+   
  });
 
  server.listen(4000);
