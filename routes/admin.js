183b37996be10a177f1848e7ab0b0462adca53bc
Rishikeshningad <ningadr@gmail.com>
2022-01-12 13:42:45 +0530

    express app till controller

--- routes/admin.js
+++ routes/admin.js
@@ -7,15 +7,14 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product',(req, res, next) => {
-    console.log('In the middleware!');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  });
-  
-// /admin/add-product => POST  
-router.post('/add-product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-  });
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
 
-  module.exports = router;
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
