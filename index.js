96b9a41e536e514142962145b80d109a5aef8017
Rishikeshningad <ningadr@gmail.com>
2022-02-04 03:25:34 +0530

    many to many association

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/User');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-Item');
 
 const app = express();
 
@@ -41,6 +43,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product); 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   
@@ -58,6 +64,9 @@ sequelize
   })
   .then(user => {
    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
   .catch(err => {
