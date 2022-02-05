64236d22848eaab6c32778551721dbe05a582b6f
Rishikeshningad <ningadr@gmail.com>
2022-02-05 16:12:10 +0530

    Toast message notifiaton

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const btn = document.getElementById('btn');
-const nav = document.getElementById('nav');
+const container = document.getElementById('container');
 
-btn.addEventListener("click", () =>{
-    nav.classList.toggle("active");
-    btn.classList.toggle("active");
+btn.addEventListener('click', () => {
+    createNotification();
 });
+
+function createNotification() {
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+
+    notif.innerText='This challenge is crazy!';
+    
+    container.appendChild(notif);
+    setTimeout(() => {
+        notif.remove();
+    },3000)
+}
